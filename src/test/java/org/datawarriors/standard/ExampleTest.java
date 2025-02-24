92178987d2dcddee5b40f1ec11926e419ec09a5a
loic_lefevre <loic.lefevre@oracle.com>
2025-02-24 17:34:46 +0100

    initial commit

---
+++ src/test/java/org/datawarriors/standard/ExampleTest.java
@@ -0,0 +1,32 @@
+package org.datawarriors.standard;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ExampleTest {
+	@Test
+	void quickTest() {
+		try (Connection c = DriverManager.getConnection(System.getProperty("jdbc.url"))) {
+			try (Statement s = c.createStatement()) {
+				try (ResultSet r = s.executeQuery("select 'Hello from database!' from dual")) {
+					if (r.next()) {
+						Assertions.assertEquals("Hello from database!",r.getString(1));
+					} else {
+						fail();
+					}
+				}
+			}
+		}
+		catch (SQLException sqle) {
+			fail(sqle);
+		}
+	}
+}
