577d3dfdb07c03942e073e067273c934df36041d
Loïc LEFEVRE <loic.lefevre@gmail.com>
2025-04-01 17:43:50 +0200

    Update database-start.sh

--- .github/database-start.sh
+++ .github/database-start.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "Managing Oracle Autonomous Database..."
-export INFO=$(curl -s -k -L -X GET "https://api.testpilotdev-controller.oraclecloud.com/ords/testpilot/admin/database?type=autonomous" -H 'accept: application/json')
+export INFO=$(curl -s -k -L -X GET "https://api.testpilot-controller.oraclecloud.com/ords/testpilot/admin/database?type=autonomous" -H 'accept: application/json')
 export HOST=$(echo $INFO | jq -r '.database' | jq -r '.host')
 export SERVICE=$(echo $INFO | jq -r '.database' | jq -r '.service')
 export PASSWORD=$(echo $INFO | jq -r '.database' | jq -r '.password')
